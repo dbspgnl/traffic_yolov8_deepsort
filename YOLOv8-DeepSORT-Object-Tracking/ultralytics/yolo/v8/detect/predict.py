f63f99bb9b5152bf08080e2178a2d59cf42a5470
swpark@yellotail.com <swpark@yellotail.com>
2023-06-01 12:33:26 +0900

    * 라벨링 세분화 및 class[ id ] speed 형태로 처리 * 라벨링 테두리 처리 및 크기 조정

--- YOLOv8-DeepSORT-Object-Tracking/ultralytics/yolo/v8/detect/predict.py
+++ YOLOv8-DeepSORT-Object-Tracking/ultralytics/yolo/v8/detect/predict.py
@@ -94,49 +94,61 @@ def compute_color_for_labels(label):
         color = [int((p * (label ** 2 - label + 1)) % 255) for p in palette]
     return tuple(color)
 
-def draw_border(img, pt1, pt2, color, thickness, r, d):
+def draw_border(img, pt1, pt2, color, thickness, r, d): # 라벨(그림 영역) 그리기
     x1,y1 = pt1
     x2,y2 = pt2
-    # Top left
-    cv2.line(img, (x1 + r, y1), (x1 + r + d, y1), color, thickness)
-    cv2.line(img, (x1, y1 + r), (x1, y1 + r + d), color, thickness)
-    cv2.ellipse(img, (x1 + r, y1 + r), (r, r), 180, 0, 90, color, thickness)
-    # Top right
-    cv2.line(img, (x2 - r, y1), (x2 - r - d, y1), color, thickness)
-    cv2.line(img, (x2, y1 + r), (x2, y1 + r + d), color, thickness)
-    cv2.ellipse(img, (x2 - r, y1 + r), (r, r), 270, 0, 90, color, thickness)
-    # Bottom left
-    cv2.line(img, (x1 + r, y2), (x1 + r + d, y2), color, thickness)
-    cv2.line(img, (x1, y2 - r), (x1, y2 - r - d), color, thickness)
-    cv2.ellipse(img, (x1 + r, y2 - r), (r, r), 90, 0, 90, color, thickness)
-    # Bottom right
-    cv2.line(img, (x2 - r, y2), (x2 - r - d, y2), color, thickness)
-    cv2.line(img, (x2, y2 - r), (x2, y2 - r - d), color, thickness)
-    cv2.ellipse(img, (x2 - r, y2 - r), (r, r), 0, 0, 90, color, thickness)
+    # Top left (점선)
+    # cv2.line(img, (x1 + r, y1), (x1 + r + d, y1), color, thickness)
+    # cv2.line(img, (x1, y1 + r), (x1, y1 + r + d), color, thickness)
+    # cv2.ellipse(img, (x1 + r, y1 + r), (r, r), 180, 0, 90, color, thickness)
+    # Top right (점선)
+    # cv2.line(img, (x2 - r, y1), (x2 - r - d, y1), color, thickness)
+    # cv2.line(img, (x2, y1 + r), (x2, y1 + r + d), color, thickness)
+    # cv2.ellipse(img, (x2 - r, y1 + r), (r, r), 270, 0, 90, color, thickness)
+    # Bottom left (점선)
+    # cv2.line(img, (x1 + r, y2), (x1 + r + d, y2), color, thickness)
+    # cv2.line(img, (x1, y2 - r), (x1, y2 - r - d), color, thickness)
+    # cv2.ellipse(img, (x1 + r, y2 - r), (r, r), 90, 0, 90, color, thickness)
+    # Bottom right (점선)
+    # cv2.line(img, (x2 - r, y2), (x2 - r - d, y2), color, thickness)
+    # cv2.line(img, (x2, y2 - r), (x2, y2 - r - d), color, thickness)
+    # cv2.ellipse(img, (x2 - r, y2 - r), (r, r), 0, 0, 90, color, thickness)
 
-    cv2.rectangle(img, (x1 + r, y1), (x2 - r, y2), color, -1, cv2.LINE_AA)
-    cv2.rectangle(img, (x1, y1 + r), (x2, y2 - r - d), color, -1, cv2.LINE_AA)
+    # 시긱 박스 원본
+    # cv2.rectangle(img, (x1 + r, y1), (x2 - r, y2), color, -1, cv2.LINE_AA)
+    # cv2.rectangle(img, (x1, y1 + r), (x2, y2 - r - d), color, -1, cv2.LINE_AA)
     
-    cv2.circle(img, (x1 +r, y1+r), 2, color, 12)
-    cv2.circle(img, (x2 -r, y1+r), 2, color, 12)
-    cv2.circle(img, (x1 +r, y2-r), 2, color, 12)
-    cv2.circle(img, (x2 -r, y2-r), 2, color, 12)
+    # 사각 박스
+    cv2.rectangle(img, (x1, y1), (x2, y2 - d - 2), color, -1, cv2.LINE_AA) # -2 줄이면 올라감
+    
+    # 원형 박스
+    # cv2.circle(img, (x1 +r, y1+r), 2, color, 12)
+    # cv2.circle(img, (x2 -r, y1+r), 2, color, 12)
+    # cv2.circle(img, (x1 +r, y2-r), 2, color, 12)
+    # cv2.circle(img, (x2 -r, y2-r), 2, color, 12)
     
     return img
 
-def UI_box(x, img, color=None, label=None, line_thickness=None):
+def UI_box(x, img, color=None, label=None, label_speed=None, label_id=None, line_thickness=None):
     # Plots one bounding box on image img
-    tl = line_thickness or round(0.002 * (img.shape[0] + img.shape[1]) / 2) + 1  # line/font thickness
-    color = color or [random.randint(0, 255) for _ in range(3)]
-    c1, c2 = (int(x[0]), int(x[1])), (int(x[2]), int(x[3]))
-    cv2.rectangle(img, c1, c2, color, thickness=tl, lineType=cv2.LINE_AA)
+    # tl = line_thickness or round(0.002 * (img.shape[0] + img.shape[1]) / 2) + 1  # line/font thickness
+    img_origin = img
+    tl = 0.0005 * (img.shape[0] + img.shape[1]) / 2 # 0.75
+    color = color or [random.randint(0, 255) for _ in range(3)] # 컬러링
+    c1, c2 = (int(x[0]), int(x[1])), (int(x[2]), int(x[3])) # 센터
+    # cv2.rectangle(img, c1, c2, color, thickness=tl, lineType=cv2.LINE_AA) # 박스(사각형)
+    # label_id = str(label_id)
     if label:
         tf = max(tl - 1, 1)  # font thickness
-        t_size = cv2.getTextSize(label, 0, fontScale=tl / 3, thickness=tf)[0]
-
-        img = draw_border(img, (c1[0], c1[1] - t_size[1] -3), (c1[0] + t_size[0], c1[1]+3), color, 1, 8, 2)
-
-        cv2.putText(img, label, (c1[0], c1[1] - 2), 0, tl / 3, [225, 255, 255], thickness=tf, lineType=cv2.LINE_AA)
+        # t_size = cv2.getTextSize(label, 0, fontScale=tl / 3, thickness=tf)[0]
+        t_label_size = cv2.getTextSize(label, 0, fontScale=tl / 3, thickness=tf)[0] # 글자 영역 사이즈
+        t_id_size = cv2.getTextSize(label_id, 0, fontScale=tl / 3, thickness=tf)[0] # 글자 영역 사이즈
+        # print(t_size)
+        img = draw_border(img, (c1[0], c1[1] - t_label_size[1] -3), (c1[0] + t_label_size[0], c1[1]+3), color, 1, 8, 2)
+        cv2.putText(img, label, (c1[0], c1[1] - 4), 0, tl / 3, [225, 255, 255], thickness=tf, lineType=cv2.LINE_AA)
+        cv2.putText(img_origin, label_id, (c1[0] + t_label_size[0], c1[1] - 4), 0, tl / 3, [225, 255, 255], thickness=tf, lineType=cv2.LINE_AA)
+        cv2.putText(img_origin, label_speed, (c1[0] + t_label_size[0] + t_id_size[0], c1[1] - 4), 0, tl / 3, [225, 255, 255], thickness=tf, lineType=cv2.LINE_AA)
+        # putText(그림, 글자, 글자 시작점, 폰트, 글자 크기 비율, 글자 색상, 글자 굵기, 글자 선형태, 그림 좌표)
 
 
 def intersect(A,B,C,D):
@@ -167,7 +179,7 @@ def get_direction(point1, point2):
 
     return direction_str
 def draw_boxes(img, bbox, names,object_id, identities=None, offset=(0, 0)):
-    cv2.line(img, line[0], line[1], (46,162,112), 3)
+    #cv2.line(img, line[0], line[1], (46,162,112), 3) # 카운팅 라인
 
     height, width, _ = img.shape
     # remove tracked point from buffer if object is lost
@@ -194,7 +206,7 @@ def draw_boxes(img, bbox, names,object_id, identities=None, offset=(0, 0)):
           speed_line_queue[id] = []
         color = compute_color_for_labels(object_id[i])
         obj_name = names[object_id[i]]
-        label = '{}{:d}'.format("", id) + ":"+ '%s' % (obj_name)
+        # label = '{}{:d}'.format("", id) + ":"+ '%s' % (obj_name)
 
         # add center to buffer
         data_deque[id].appendleft(center)
@@ -203,7 +215,7 @@ def draw_boxes(img, bbox, names,object_id, identities=None, offset=(0, 0)):
           object_speed = estimatespeed(data_deque[id][1], data_deque[id][0])
           speed_line_queue[id].append(object_speed)
           if intersect(data_deque[id][0], data_deque[id][1], line[0], line[1]):
-              cv2.line(img, line[0], line[1], (255, 255, 255), 3)
+            #   cv2.line(img, line[0], line[1], (255, 255, 255), 3) # 카운팅 라인 지날 때 색상 처리
             #   if "South" in direction:
               if "West" in direction:
                 if obj_name not in object_counter:
@@ -217,37 +229,36 @@ def draw_boxes(img, bbox, names,object_id, identities=None, offset=(0, 0)):
                 else:
                     object_counter1[obj_name] += 1
 
+        label_speed = ""
+        label_id = '[ %d ]' % (id)
         try:
-            label = label + " " + str(sum(speed_line_queue[id])//len(speed_line_queue[id])) + "km/h"
+            # label = label + " " + str(sum(speed_line_queue[id])//len(speed_line_queue[id])) + "km/h"
+            label_speed = " " + str(sum(speed_line_queue[id])//len(speed_line_queue[id])) + "km"
         except:
             pass
-        UI_box(box, img, label=label, color=color, line_thickness=2)
-        # draw trail
-        for i in range(1, len(data_deque[id])):
-            # check if on buffer value is none
-            if data_deque[id][i - 1] is None or data_deque[id][i] is None:
-                continue
-            # generate dynamic thickness of trails
-            thickness = int(np.sqrt(64 / float(i + i)) * 1.5)
-            # draw trails
-            cv2.line(img, data_deque[id][i - 1], data_deque[id][i], color, thickness)
-    
-    #4. Display Count in top right corner
-        for idx, (key, value) in enumerate(object_counter1.items()):
-            cnt_str = str(key) + ":" +str(value)
-            cv2.line(img, (width - 500,25), (width,25), [85,45,255], 40)
-            cv2.putText(img, f'Number of Vehicles Entering', (width - 500, 35), 0, 1, [225, 255, 255], thickness=2, lineType=cv2.LINE_AA)
-            cv2.line(img, (width - 150, 65 + (idx*40)), (width, 65 + (idx*40)), [85, 45, 255], 30)
-            cv2.putText(img, cnt_str, (width - 150, 75 + (idx*40)), 0, 1, [255, 255, 255], thickness = 2, lineType = cv2.LINE_AA)
+        UI_box(box, img, label=obj_name, label_speed=label_speed, label_id=label_id, color=color, line_thickness=None) # 라벨링 박스 그리기
 
-        for idx, (key, value) in enumerate(object_counter.items()):
-            cnt_str1 = str(key) + ":" +str(value)
-            cv2.line(img, (20,25), (500,25), [85,45,255], 40)
-            cv2.putText(img, f'Numbers of Vehicles Leaving', (11, 35), 0, 1, [225, 255, 255], thickness=2, lineType=cv2.LINE_AA)    
-            cv2.line(img, (20,65+ (idx*40)), (127,65+ (idx*40)), [85,45,255], 30)
-            cv2.putText(img, cnt_str1, (11, 75+ (idx*40)), 0, 1, [225, 255, 255], thickness=2, lineType=cv2.LINE_AA)
+        # # draw trail
+        # for i in range(1, len(data_deque[id])):
+        #     if data_deque[id][i - 1] is None or data_deque[id][i] is None: # check if on buffer value is none
+        #         continue
+        #     thickness = int(np.sqrt(64 / float(i + i)) * 1.5) # generate dynamic thickness of trails
+        #     cv2.line(img, data_deque[id][i - 1], data_deque[id][i], color, thickness) # draw trails
     
-    
+        #4. Display Count in top right corner
+        # for idx, (key, value) in enumerate(object_counter1.items()):
+        #     cnt_str = str(key) + ":" +str(value)
+        #     cv2.line(img, (width - 500,25), (width,25), [85,45,255], 40)
+        #     cv2.putText(img, f'Number of Vehicles Entering', (width - 500, 35), 0, 1, [225, 255, 255], thickness=2, lineType=cv2.LINE_AA)
+        #     cv2.line(img, (width - 150, 65 + (idx*40)), (width, 65 + (idx*40)), [85, 45, 255], 30)
+        #     cv2.putText(img, cnt_str, (width - 150, 75 + (idx*40)), 0, 1, [255, 255, 255], thickness = 2, lineType = cv2.LINE_AA)
+
+        # for idx, (key, value) in enumerate(object_counter.items()):
+        #     cnt_str1 = str(key) + ":" +str(value)
+        #     cv2.line(img, (20,25), (500,25), [85,45,255], 40)
+        #     cv2.putText(img, f'Numbers of Vehicles Leaving', (11, 35), 0, 1, [225, 255, 255], thickness=2, lineType=cv2.LINE_AA)    
+        #     cv2.line(img, (20,65+ (idx*40)), (127,65+ (idx*40)), [85,45,255], 30)
+        #     cv2.putText(img, cnt_str1, (11, 75+ (idx*40)), 0, 1, [225, 255, 255], thickness=2, lineType=cv2.LINE_AA)
     
     return img
 
@@ -317,7 +328,7 @@ class DetectionPredictor(BasePredictor):
             oids.append(int(cls))
         xywhs = torch.Tensor(xywh_bboxs)
         confss = torch.Tensor(confs)
-          
+
         outputs = deepsort.update(xywhs, confss, oids, im0)
         if len(outputs) > 0:
             bbox_xyxy = outputs[:, :4]
